4fb270dedaac3ae466278ef9f2be7d83fd2448e0
Amar Cutura <amar.cutura@gmail.com>
2025-12-19 22:53:58 +0100

    Add engine state

--- src/pyvenue/engine/engine.py
+++ src/pyvenue/engine/engine.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 
 from pyvenue.engine.orderbook import OrderBook
+from pyvenue.engine.state import EngineState
 from pyvenue.infra import Clock, SystemClock
 
 
@@ -10,6 +11,7 @@ from pyvenue.infra import Clock, SystemClock
 class Engine:
     clock: Clock = field(default_factory=SystemClock)
     book: OrderBook = field(default_factory=OrderBook)
+    state: EngineState = field(default_factory=EngineState)
 
     def submit(self, command: object) -> list[object]:
         raise NotImplementedError
